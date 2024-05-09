9f6658255563d0c77b04061fca6f95a752ec7d75
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-05-09 21:39:02 +0000

    introducing PSU classes

---
+++ psudriver.py
@@ -0,0 +1,99 @@
+from enum import Enum
+from pyvisa.constants import StopBits, Parity
+from pyvisa import constants
+
+class PSUModel(Enum):
+    E3649A = 'E3649A',
+    E3631A = 'E3631A',            
+
+class PSUDevice:
+
+    def __init__(self, model: PSUModel, session):
+        if not isinstance(model, PSUModel):
+            raise TypeError
+
+        self.model = model
+        self.session = session
+        self.modelname = self.model.name
+
+        if model == PSUModel.E3649A:
+            self.brand = 'Keysight'
+            self.nchannels = 2
+        elif model == PSUModel.E3631A:
+            self.brand = 'Agilent'
+            self.nchannels = 3
+
+        self.settings = {
+            "brand" : self.brand,
+            "model": self.modelname,
+            "output": False,
+        }
+
+    def getSettingsSchema(self):
+        self.settings["vset"] = [0.0] * self.nchannels
+        self.settings["ilimit"] = [0.0] * self.nchannels
+        return self.settings
+
+    def getVoltage(self, channel):
+        self.channel = channel
+        return self.voltage
+
+    def setVoltage(self, channel, value):
+        self.channel = channel
+        self.vset = value
+
+    def getCurrent(self, channel):
+        self.channel = channel
+        return self.current
+
+    def setCurrentLimit(self, channel, value):
+        self.channel = channel
+        self.ilimit = value
+
+    def getCurrentLimit(self, channel):
+        return self.ilimit
+
+    @property
+    def output(self):
+        return int(self.session.query('OUTPUT:STATE?'))
+
+    @output.setter
+    def output(self, value):
+        if value in ['ON', 'OFF'] or value in [0, 1]:
+            self.session.write(f'OUTPUT:STATE {value}')
+
+    @property
+    def channel(self):
+        s = self.session.query('INST:NSEL?')
+        return int(s[-1])
+
+    @channel.setter
+    def channel(self, value):
+        if value in range(1, self.nchannels + 1):
+            self.session.write(f'INST:NSEL {value}')
+
+    @property
+    def voltage(self):
+        return float(self.session.query('MEAS?'))
+
+    @property
+    def vset(self):
+        return float(self.session.query('VOLT?'))
+
+    @vset.setter
+    def vset(self, value):
+        self.session.write(f'VOLT {value}')
+
+    @property
+    def current(self):
+        return float(self.session.query('MEAS:CURR?'))
+
+    @property
+    def ilimit(self):
+        return float(self.session.query('CURR?'))
+
+    @vset.setter
+    def ilimit(self, value):
+        self.session.write(f'CURR {value}')
+
+
--- psufe.py
+++ psufe.py
@@ -7,18 +7,16 @@ import pyvisa
 from pyvisa.constants import StopBits, Parity
 from pyvisa import constants
 
+from psudriver import PSUModel, PSUDevice
+
 class PSU(midas.frontend.EquipmentBase):
 
     def __init__(self, client, session, model):
 
         self.session = session
-        self.model = model
+        self.psu = PSUDevice(PSUModel[model], session)
 
-        if(midas.frontend.frontend_index == -1):
-            client.msg("set frontend index with -i option", is_error=True)
-            sys.exit(-1)
-
-        equip_name = f'PSU-{model}-{str(midas.frontend.frontend_index).zfill(2)}'
+        equip_name = f'PSU{str(midas.frontend.frontend_index).zfill(2)}-{model}'
 
         default_common = midas.frontend.InitialEquipmentCommon()
         default_common.equip_type = midas.EQ_PERIODIC
@@ -31,21 +29,36 @@ class PSU(midas.frontend.EquipmentBase):
 
         midas.frontend.EquipmentBase.__init__(self, client, equip_name, default_common);
 
+        # FIXME
+        self.psu.setVoltage(1, 2.5)
+        self.psu.setCurrentLimit(1, 0.25)
+        self.psu.setVoltage(2, 5.0)
+        self.psu.setCurrentLimit(2, 0.50)
+        self.psu.output = 1
+
     def readout_func(self):
-        None
+        for ch in range(1, self.psu.nchannels+1):
+            print(f'OUT{ch}: {self.psu.output}')
+            print(f'V{ch}: {self.psu.getVoltage(ch)}')
+            print(f'I{ch}: {self.psu.getCurrent(ch)}')
+            print(f'ILIM{ch}: {self.psu.getCurrentLimit(ch)}')
 
 class PSUFrontend(midas.frontend.FrontendBase):
 
     def __init__(self, session, model):
-        midas.frontend.FrontendBase.__init__(self, "PSU")
+        if(midas.frontend.frontend_index == -1):
+            client.msg("set frontend index with -i option", is_error=True)
+            sys.exit(-1)
+        midas.frontend.FrontendBase.__init__(self, f"PSU{str(midas.frontend.frontend_index).zfill(2)}")
         self.add_equipment(PSU(self.client, session, model))
 
 if __name__ == "__main__":
     parser = midas.frontend.parser
     parser.add_argument("--port", default="/dev/ttyUSB0")
-    parser.add_argument("--model", required=True, choices = ['E3649A', 'E3631A'])
+    parser.add_argument("--model", required=True, choices = [m.name for m in PSUModel])
     args = midas.frontend.parse_args()
 
+    # lookup for PSU on USB port
     rm = pyvisa.ResourceManager()
     dev = f'ASRL{args.port}::INSTR'
     session = rm.open_resource(dev, baud_rate = 9600, data_bits = 7, parity = Parity.even, 
@@ -64,6 +77,22 @@ if __name__ == "__main__":
     else:
         print(f"E: PSU {args.model} not found on {args.port}")
         sys.exit(-1)
+
+    equip_name = f'PSU{str(midas.frontend.frontend_index).zfill(2)}-{model}'
+
+    # check if a PSU frontend is running with same model and id
+    client = midas.client.MidasClient("psu")
+
+    if client.odb_exists(f"/Equipment/{equip_name}/Common/Frontend name"):
+        fename = client.odb_get(f"/Equipment/{equip_name}/Common/Frontend name")
+
+        if client.odb_get(f"/Equipment/{equip_name}"):
+            for cid in client.odb_get(f'/System/Clients'):
+                if client.odb_get(f'/System/Clients/{cid}/Name') == fename:
+                    client.msg(f"{equip_name} already running on MIDAS server, please change frontend index")
+                    sys.exit(-1)
+
+    client.disconnect()
 
     fe = PSUFrontend(session, model)
     fe.run()
