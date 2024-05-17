eedcb0919b66e16781435a9cbdfc46ba51c30ab2
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-05-17 13:03:20 +0000

    add spikes directory

---
+++ spikes/list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import warnings
+warnings.simplefilter('ignore')
+
+from pyvisa import ResourceManager, constants
+
+class PSU:
+    def __init__(self, session):
+        self.session = session
+
+    def idn(self):
+        return self.session.query('*IDN?')
+
+
+pslist = []
+rm = ResourceManager()
+
+for resource in rm.list_resources():
+    if rm.resource_info(resource).interface_type == constants.InterfaceType.asrl:
+        print("OK")
+        session = rm.open_resource(resource, baud_rate = 9600, data_bits = 7, parity = constants.Parity.even,
+                flow_control = constants.VI_ASRL_FLOW_NONE, stop_bits = constants.StopBits.two)
+        session.read_termination = '\r\n'
+        session.write_termination = '\n'
+
+        pslist.append(PSU(session))
+
+for psu in pslist:
+    print(psu.idn())
